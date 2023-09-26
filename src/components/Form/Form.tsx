b43d2a078667fe4373c051826d283fae87258129
Albinator <albinamivina@gmail.com>
2023-09-26 14:44:11 +0300

    add formik

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,99 +1,81 @@
-import {Input, FormLabel, Radio, Checkbox} from '@mui/joy';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { Input, Checkbox} from '@mui/joy';
 import Key from '@mui/icons-material/Key';
 import MailIcon from '@mui/icons-material/Mail';
 import Button from '../Button/Button';
 import PhoneInput from 'react-phone-input-2';
+import RadioGroup from '../RadioGroup/RadioGroup';
 
 import 'react-phone-input-2/lib/style.css'
+
 import style from './styles.module.css';
 
-function Form ({type}: { type: string }) {
+function MyForm ({type}: { type: string }) {
+	const validationSchema = Yup.object().shape({
+		email: Yup.string()
+			.email('Invalid email address')
+			.required('Email is required'),
+		password: Yup.string()
+			.min(6, 'Password must be at least 6 characters')
+			.required('Password is required'),
+		// Add validation for other fields as needed
+	});
 	return (
-		<form action="">
-			<FormLabel sx={{
-				fontSize: '18px',
-				marginBottom: '10px'
-			}}>Login</FormLabel>
-			<Input
-				placeholder="mail@mui.com"
-				type="email"
-				startDecorator={<MailIcon />}
-				required
-				sx={{
-					marginBottom: '30px'
-				}}
+		<Formik 
+			initialValues={{
+			email: '',
+			password: '',
+			}}
+			onSubmit={(values, { setSubmitting }) => {
+				console.log(values);
+			}}
+			validationSchema={validationSchema}>
+			<Form>
+			<label className={style.formLabel}>Login</label>
+			<Field
+				type="email" name="email" placeholder=""
+				component={({ field }) => (
+					<Input {...field} startDecorator={<MailIcon />} placeholder='email@gmail.com'/>
+				)}
 			/>
-			<FormLabel sx={{
-				fontSize: '18px',
-				marginBottom: '10px'
-			}}>Password</FormLabel>
-			<Input
+			<label>Password</label>
+			<Field
 				type="password"
-				placeholder="Type in here…"
-				startDecorator={<Key />}
-				required
+				name="password"
+				component={({ field }) => (
+					<Input {...field} startDecorator={<Key />}/>
+				)}
 			/>
 			{type === 'Sign up'? (
 				<>
-					<FormLabel sx={{
-						fontSize: '18px',
-						marginBottom: '10px',
-						marginTop: '10px'
-					}}>Password</FormLabel>
-					<Input
+					<label>Password</label>
+					<Field
 						type="password"
-						placeholder="Type in here…"
-						startDecorator={<Key />}
-						required
+						name="password"
+						component={({ field }) => (
+							<Input {...field} startDecorator={<Key />}/>
+						)}
 					/>
-					<FormLabel sx={{
-						fontSize: '18px',
-						marginBottom: '10px',
-						marginTop: '10px'
-					}}>Phone number</FormLabel>
-					<PhoneInput
-						country={'ua'}
+					<label>Phone number</label>
+					<Field
+						type="tel"
+						name="tel"
+						component={({ field }) => (
+							<PhoneInput {...field} country={'ua'} />
+						)}
 					/>
-					<FormLabel sx={{
-						fontSize: '18px',
-						marginBottom: '10px',
-						marginTop: '10px',
-						textAlign: 'center',
-						display: 'block'
-					}}>Sex</FormLabel>
-					<div className={style.radio}>
-						<Radio
-							// checked={selectedValue === 'a'}
-							// onChange={handleChange}
-							value="Male"
-							name="radio-buttons"
-							slotProps={{ input: { 'aria-label': 'Male' } }}
-							label='Male'
-							sx={{
-								color: 'black'
-							}}
-						/>
-						<Radio
-							// checked={selectedValue === 'b'}
-							// onChange={handleChange}
-							value="Female"
-							label='Female'
-							name="radio-buttons"
-							slotProps={{ input: { 'aria-label': 'Female' } }}
-							sx={{
-								color: 'black',
-								marginBottom: '10px'
-							}}
-						/>
-					</div>
+					<label>Sex</label>
+					<Field name="gender" component={RadioGroup} />
 					<hr style={{marginBottom: '30px'}}/>
 					<Checkbox label="I agree with Private policy" required/>
 				</>
 			): undefined}
 			{type === 'Sign in'? <a href="#" className={style.forgot}>Forgot password?</a>: undefined}
 			<Button appearance='filled' type='submit' className={style.buttonSubmit}>Submit</Button>
-		</form>
+		</Form>
+		</Formik>
 	)
 }
 
-export default Form;
+export default MyForm;
